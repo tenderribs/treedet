3a5f5ce3db5f2c5cd3b77ab8fb8bf3596bdc15e3
tenderribs <dc3dude@gmail.com>
2024-03-18 21:29:55 +0100

    save nice commands

---
+++ commands.sh
@@ -0,0 +1,18 @@
+# batch size 1, # mixed precision, occupy memory in advance
+python3 -m yolox.tools.train -n yolox_s_tree_pose --task human_pose --dataset tree_kpts -b 32 --fp16 --occupy\
+    -c './pretrained_models/yolox-s-ti-lite_39p1_57p9_checkpoint.pth' --workers 12
+
+# export onnx
+python3 tools/export_onnx.py \
+    --output-name pretrained_models/yolox_s_tree_pose.onnx \
+    -f exps/default/yolox_s_tree_pose.py \
+    --task human_pose \
+    --export-det \
+    -c YOLOX_outputs/yolox_s_tree_pose/best_ckpt.pth
+
+# run inference
+python3 kpts_onnx_inference.py \
+    --model pretrained_models/yolox_s_tree_pose.onnx \
+    --output_dir YOLOX_outputs/inference \
+    --score_thr 0.8 \
+    --image_path ./datasets/mark_forest/IMG_20240318_124218.png
