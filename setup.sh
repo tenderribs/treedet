72545a5f78f9f7fca21d894da7918f531d707b87
tenderribs <dc3dude@gmail.com>
2024-03-26 19:12:58 +0000

    fix devcontainer build

--- setup.sh
+++ setup.sh
@@ -31,19 +31,18 @@
 ######################################################################
 
 # system packages
-sudo apt-get install -y libjpeg-dev zlib1g-dev cmake libffi-dev protobuf-compiler
+sudo apt-get install -y libjpeg-dev zlib1g-dev cmake libffi-dev
 
-######################################################################
 # upgrade pip
 pip3 install --no-input --upgrade pip setuptools
 
-######################################################################
 echo "installing pytorch - use the applopriate index-url from https://pytorch.org/get-started/locally/"
 
 # unfortunately needs to be nightly build stable torch binaries aren't built for "new" sm_89 architecture which includes my RTX 4070 (ada gen)
 pip3 install --pre torch torchvision torchaudio --index-url https://download.pytorch.org/whl/nightly/cu121 # install more recent version of torch compiled against cuda 12.1
 
 echo 'Installing python packages...'
+
 # there as issue with installing pillow-simd through requirements - force it here
 pip uninstall --yes pillow
 pip install --no-input -U --force-reinstall pillow-simd
@@ -53,22 +52,6 @@ pip3 install --no-input torchinfo pycocotools opencv-python
 echo "installing requirements"
 pip3 install --no-input -r requirements.txt
 
-######################################################################
-echo "Installing mmcv"
-# pip3 install --no-input mmcv-full==1.4.8 -f https://download.openmmlab.com/mmcv/dist/cu118/torch2.0.1/index.html
-# kinda sus because actually using torch v2.2, not v2.1 but whatever this project is called yolo for a reason
-# pip3 install --no-input mmcv==2.1.0 -f https://download.openmmlab.com/mmcv/dist/cu121/torc==1.23.0h2.1/index.html # newer version
-
-######################################################################
-# can we move this inside the requirements file is used.
-# pip3 install --no-input protobuf==3.20.2 onnx==1.13.0
-pip3 install --no-input protobuf==3.20.3
-
-# add build flag, otherwise problems https://github.com/onnx/onnx/issues/4704
-export CMAKE_ARGS="-DONNX_USE_PROTOBUF_SHARED_LIBS=ON"
-pip3 install --no-input onnx
-
-######################################################################
 echo 'installing the python package...'
 python3 setup.py develop
 
