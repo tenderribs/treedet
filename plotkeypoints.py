0d51088fcc1eb6d9c88bec5c7f9453e0e84ba7e4
tenderribs <dc3dude@gmail.com>
2024-04-07 22:55:06 +0200

    print bbox

--- plotkeypoints.py
+++ plotkeypoints.py
@@ -2,22 +2,30 @@ import cv2
 import os
 import json
 
+
 # supposed names of the keypoints
 keypoint_names = ["kpC", "kpL", "kpR", "AX1", "AX2"]
 
-with open('./datasets/SynthTree43k/annotations/trees_val.json') as json_file:
+with open('./datasets/CanaTree100/annotations/trees_val.json') as json_file:
     data = json.load(json_file)
-    image = data["images"][3]
+    image = data["images"][0]
 
     id = image['id']
 
     # get a list of annotations for the image
     annotations = [ann for ann in data["annotations"] if ann["image_id"] == id]
 
-    image_filename = os.path.join("./datasets/SynthTree43k", image['file_name'])
+    image_filename = os.path.join("./datasets/CanaTree100", image['file_name'])
     cvimage = cv2.imread(image_filename)
 
     for tree in annotations:
+        xmin, ymin, w, h = tree['bbox']
+
+        p1 = (int(xmin), int(ymin))
+        p2 = (int(xmin + w), int(ymin + h))
+
+        cv2.rectangle(cvimage, p1, p2, (52, 64, 235), 1)
+
         kp = tree['keypoints']
         for idx in range(5):
             x = int(kp[0 + idx*3])
