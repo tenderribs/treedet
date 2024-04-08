d12f7b0df002d4193f3cdf20d2df745749882c63
tenderribs <dc3dude@gmail.com>
2024-04-08 09:01:43 +0000

    add mean and std for canawiki

--- config.py
+++ config.py
@@ -12,8 +12,8 @@ datasets = {
         "train_ann": "CanaWikiTree200_train.json",
         "test_ann": "CanaWikiTree200_val.json", # use val for testing since only 200 images
         "val_ann": "CanaWikiTree200_val.json",
-        "mean_bgr": [63.9591, 80.7588, 89.1241],
-        "std_bgr": [56.9384, 58.6090, 63.3794]
+        "mean_bgr": [83.0013, 93.8874, 96.4963],
+        "std_bgr": [55.7843, 56.4000, 56.7816]
     }
 }
 
