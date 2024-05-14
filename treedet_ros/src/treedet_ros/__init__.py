91ad2e9373325c8576a525cb0170d60f8f56a0a2
tenderribs <dc3dude@gmail.com>
2024-05-14 17:01:49 +0200

    move around rviz boilerplate

--- treedet_ros/src/treedet_ros/__init__.py
+++ treedet_ros/src/treedet_ros/__init__.py
@@ -3,3 +3,4 @@ from .cutting_data import get_cutting_data, uv2xyz
 from .icp import icp
 from .pcl import pc1, pc2, kp1, kp2, set_axes_equal
 from .sort import Sort
+from .rviz import point_markers, np_to_markers
